23b45b8f82666c4c6a81b08f853c22efcf49bb45
jeffsonsenior <kinyuajeff@gmail.com>
2024-06-28 21:00:49 +0000

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# install puppet-lint. Version must be 2.5.0
-exec { 'puppet-lint':
-  command => '/bin/gem install puppet-lint -v 2.5.0',
+# install puppet-lint
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
